c282f1b38e3c5b0c0079ab42a3174032fb750f79
Gaurav <gaurav.jain2@soprasteria.com>
2021-08-10 15:57:07 +0530

    we learn how to use promp and different ways to do that

--- src/Bookstore.js
+++ src/Bookstore.js
@@ -1,32 +1,50 @@
 import React from 'react'
 import './index.css'
 
+const FirstBook ={
+
+    title: "Love Bytes",
+    author: "Vandana Vaadera",
+    img: "https://images-eu.ssl-images-amazon.com/images/I/418Ucei0LcS._AC_SX184_.jpg"
+}
+const SecondBook ={
+
+    title: "Gods & Monsters",
+    author: "Shelby Mahurin",
+    img: "https://images-eu.ssl-images-amazon.com/images/I/51WM8Q+vfKS._AC_SX184_.jpg"
+
+}
+
+const ThirdBook ={
+    
+    title: "Man Enough: Undefining My Masculinity",
+    author: "Justin Baldon",
+    img: "https://images-eu.ssl-images-amazon.com/images/I/51ud1l90zhL._AC_SX184_.jpg"
+}
+
 function Bookstore() {
     return (
         <section className ="booklist">
 
-         <Book />
-         <Book />
-         <Book />
-        
-
+         <Book img={FirstBook.img} title={FirstBook.title} author={FirstBook.author} />  
+         {/* we have passed in the object through the component using props */}
+         <Book img={SecondBook.img} title={SecondBook.title} author={SecondBook.author}></Book>
+         <Book img={ThirdBook.img} title = {ThirdBook.title}  author = {ThirdBook.author}></Book>
+    
         </section>
            
     )
 }
-const Book = () => {
+const Book = ({img,title,author}) => {
 
-    return <article>
-        
-        <Img />
-        <Title />
-        <Author />
+    // const {img, title, author} = props  // another way to use props
 
+    return (<article className = 'book'>
+
+        <img src ={img}></img>
+        <h1>{title}</h1>
+        <h3>{author}</h3>
     </article>
+   );
 }
-
-const Img = () => <img src="https://images-na.ssl-images-amazon.com/images/I/41EANP48VAL._SX322_BO1,204,203,200_.jpg" alt ="" ></img>
-const Title = () => <h1 style= {{color: '#FFFF00', fontSize: '1 rem', marginTop: '0.50rem'}}> Alex Michalides</h1>
-const Author = () => <h3 style = {{color: '#FFA500', fontSize:'1 rem', marginTop:"0.25rem", letterSpacing: "2.5px"}}> Ruskin Bond </h3>
-
 export default Bookstore
