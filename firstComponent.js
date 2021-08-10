35309802668c4a6d846effa97c0ba4619a775824
Gaurav <gaurav.jain2@soprasteria.com>
2021-08-10 09:57:25 +0530

    First Component JS

---
+++ firstComponent.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { render } from "react-dom";
+
+function FirstComponent()
+{
+    return(
+
+       <React.Fragment>
+
+      <div>
+           
+      </div>
+
+      <div>
+
+          <h2>  This is my First component</h2>
+      </div>
+
+
+       </React.Fragment>
+    )
+}
+
+export default FirstComponent
