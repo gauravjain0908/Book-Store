e2e0b481d0328b9f0b62ea90fc20824473dcba12
Gaurav <gaurav.jain2@soprasteria.com>
2021-08-10 14:55:49 +0530

    index Css

--- src/Bookstore.js
+++ src/Bookstore.js
@@ -1,29 +1,32 @@
 import React from 'react'
+import './index.css'
 
 function Bookstore() {
     return (
-        <section>
+        <section className ="booklist">
 
          <Book />
          <Book />
          <Book />
-         <Book />
-         <Book />
-         <Book />
+        
 
         </section>
            
     )
 }
-
 const Book = () => {
 
     return <article>
-        <Img>
         
-        </Img>
+        <Img />
+        <Title />
+        <Author />
+
     </article>
 }
 
-const Img = () => <img src="https://www.amazon.in/dp/1409181677/ref=s9_acsd_al_bw_c2_x_5_i?pf_rd_m=A1K21FY43GMZF8&pf_rd_s=merchandised-search-3&pf_rd_r=Y42B7TXZ8NTTJR745340&pf_rd_t=101&pf_rd_p=547173bb-3d8d-4795-bdfd-58060a6dba22&pf_rd_i=976389031"> </img>
+const Img = () => <img src="https://images-na.ssl-images-amazon.com/images/I/41EANP48VAL._SX322_BO1,204,203,200_.jpg" alt ="" ></img>
+const Title = () => <h1 style= {{color: '#FFFF00', fontSize: '1 rem', marginTop: '0.50rem'}}> Alex Michalides</h1>
+const Author = () => <h3 style = {{color: '#FFA500', fontSize:'1 rem', marginTop:"0.25rem", letterSpacing: "2.5px"}}> Ruskin Bond </h3>
+
 export default Bookstore
--- src/index.js
+++ src/index.js
@@ -3,17 +3,16 @@ import ReactDOM from 'react-dom';
 import Bookstore from './Bookstore';
 // import FirstComponent from "./firstComponent";  // here we have imported our first component 
 // import SecondComponent from "./SecondComponent";
-import NestedComponent from "./NestedComponent";
-import Bookstore from "./Bookstore";
+ //import NestedComponent from "./NestedComponent";
 
 
 ReactDOM.render(
 
 <React.StrictMode>
     
-    <Bookstore/>
-    <NestedComponent />
-    
+    <Bookstore />
+
+     
   
   </React.StrictMode>,
   
